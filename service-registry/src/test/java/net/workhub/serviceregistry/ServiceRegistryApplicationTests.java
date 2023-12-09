aeb6e6d9f1455d79f89d53ed13ef27e6da094bf8
xiao-yangg <xiaoyangg2020@outlook.com>
2023-12-10 00:07:22 +0800

    feat: load balancer with spring cloud eureka server

---
+++ service-registry/src/test/java/net/workhub/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.workhub.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
